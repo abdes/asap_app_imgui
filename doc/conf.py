381a3312bf8acc1129c2c7c46fd070d833249b57
Abdessattar Sassi <abde.sassi@gmail.com>
2018-10-04 14:33:10 +0400

    updated filesystem submodule

--- doc/conf.py
+++ doc/conf.py
@@ -26,7 +26,7 @@ author = u'Abdessattar Sassi (abde.sassi gmail account)'
 # The short X.Y version
 version = u'0.1.1'
 # The full version, including alpha/beta/rc tags
-release = u'0.1.1 (60ca8f40220a)'
+release = u'0.1.1 (e402be453179)'
 
 rst_prolog = """
 .. |version| replace:: {0}
