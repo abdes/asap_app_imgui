46cd6faa22534e5914038c2d3376c9c0b8762106
Abdessattar Sassi <abde.sassi@gmail.com>
2018-11-25 19:09:37 +0400

    although ImGui demo uses GL version 3.0, we need 3.2 for the demo shaders to work well with glfw

--- main/src/imgui_runner.cpp
+++ main/src/imgui_runner.cpp
@@ -65,11 +65,11 @@ void ImGuiRunner::InitGraphics() {
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);  // Required on Mac
 #else
-  // GL 3.0
+  // GL 3.2 (can be set to lower if required, up to 3.0)
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-  // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+
-  // only glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // 3.0+ only
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // 3.0+ only
 #endif
 
   ASLOG(debug, "  GLFW init done");
