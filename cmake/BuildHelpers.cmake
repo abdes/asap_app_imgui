146c6acc27b3bed2cda36b04bd682e25078653ba
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2021-11-04 12:13:11 +0400

    refactor: source grouping no longer used

--- cmake/BuildHelpers.cmake
+++ cmake/BuildHelpers.cmake
@@ -382,33 +382,3 @@ function(asap_test_executable)
   endif()
 
 endfunction()
-
-# Define function "source_group_by_path with three mandatory arguments
-# (PARENT_PATH, REGEX, GROUP, ...) to group source files in folders (e.g. for
-# MSVC solutions).
-#
-# Example:
-# ~~~
-# source_group_by_path(
-#   "${CMAKE_CURRENT_SOURCE_DIR}/src"
-#   "\\\\.h$|\\\\.inl$|\\\\.cpp$|\\\\.c$|\\\\.ui$|\\\\.qrc$" "Source Files"
-#   ${sources})
-# ~~~
-function(source_group_by_path PARENT_PATH REGEX GROUP)
-
-  foreach(FILENAME ${ARGN})
-
-    get_filename_component(FILEPATH "${FILENAME}" REALPATH)
-    file(RELATIVE_PATH FILEPATH ${PARENT_PATH} ${FILEPATH})
-    get_filename_component(FILEPATH "${FILEPATH}" DIRECTORY)
-
-    string(REPLACE "/" "\\" FILEPATH "${FILEPATH}")
-
-    source_group(
-      "${GROUP}\\${FILEPATH}"
-      REGULAR_EXPRESSION "${REGEX}"
-      FILES ${FILENAME})
-
-  endforeach()
-
-endfunction(source_group_by_path)
