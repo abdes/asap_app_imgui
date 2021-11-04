e19ec292e9ee68350e7e37890fec215fc7a6c622
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2021-11-04 23:39:30 +0400

    refactor: clean google sanitizers options

--- cmake/GoogleSanitizers.cmake
+++ cmake/GoogleSanitizers.cmake
@@ -10,21 +10,33 @@
 # ------------------------------------------------------------------------------
 
 if(OPTION_GOOGLE_ASAN)
-  set(ASAN_COMPILER_FLAGS
-      "-g -O1 -fuse-ld=gold -fno-omit-frame-pointer -fsanitize=address -fsanitize=leak"
-  )
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${ASAN_COMPILER_FLAGS}")
-  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${ASAN_COMPILER_FLAGS}")
+  if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    # using Clang or GNU compilers
+    set(ASAN_COMPILER_FLAGS "-fno-omit-frame-pointer -fsanitize=address -fsanitize=leak"
+    )
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${ASAN_COMPILER_FLAGS}")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${ASAN_COMPILER_FLAGS}")
+  else()
+    message(FATAL_ERROR "Google Address Sanitizer can only be used with clang or gnu compilers")
+  endif()
 endif()
 
 if(OPTION_GOOGLE_USAN)
-  set(USAN_COMPILER_FLAGS "-fuse-ld=gold -fsanitize=undefined")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${USAN_COMPILER_FLAGS}")
-  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${USAN_COMPILER_FLAGS}")
+  if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+    set(USAN_COMPILER_FLAGS "-fsanitize=memory -fPIE -pie -Qunused-arguments -fsanitize-memory-track-origins")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${USAN_COMPILER_FLAGS}")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${USAN_COMPILER_FLAGS}")
+  else()
+    message(FATAL_ERROR "Google Memory Sanitizer can only be used with clang compiler")
+  endif()
 endif()
 
 if(OPTION_GOOGLE_TSAN)
-  set(TSAN_COMPILER_FLAGS "-fuse-ld=gold -fsanitize=thread")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${TSAN_COMPILER_FLAGS}")
-  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${TSAN_COMPILER_FLAGS}")
+  if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    set(TSAN_COMPILER_FLAGS "-fsanitize=thread")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${TSAN_COMPILER_FLAGS}")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${TSAN_COMPILER_FLAGS}")
+  else()
+    message(FATAL_ERROR "Google Address Sanitizer can only be used with clang or gnu compilers")
+  endif()
 endif()
