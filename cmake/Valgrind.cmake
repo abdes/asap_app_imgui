7c24eb8123f3adb8f1c98ea8a799534584a79ee1
Abdessattar Sassi <457645+abdes@users.noreply.github.com>
2022-09-18 19:11:34 +0400

    refactor: remove unused code

--- cmake/Valgrind.cmake
+++ cmake/Valgrind.cmake
@@ -4,19 +4,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # ===-----------------------------------------------------------------------===#
 
-include(common/Valgrind)
-
 mark_as_advanced(${PROJECT_NAME}_ENABLE_PROFILING)
 set(${PROJECT_NAME}_ENABLE_PROFILING ${ASAP_WITH_VALGRIND})
 
-function(asap_add_valgrind_memcheck target)
-  swift_add_valgrind_memcheck(${target}, ${ARGN})
-endfunction()
-
-function(asap_add_valgrind_callgrind target)
-  swift_add_valgrind_callgrind(${target}, ${ARGN})
-endfunction()
-
-function(asap_add_valgrind_massif target)
-  swift_add_valgrind_massif(${target}, ${ARGN})
-endfunction()
+include(common/Valgrind)
